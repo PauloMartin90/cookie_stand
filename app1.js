6422e6c813d83e760e4b1b54e590860f3cf68db8
Paulo Martin <paumar559@gmail.com>
2021-01-23 18:06:08 -0800

    Merging Branch

--- app1.js
+++ app1.js
@@ -1,14 +1,13 @@
  'use strict';
 
-var body = document.getElementById('table-body');
-var submitEl = document.getElementById('Submit');
-var locationEl = document.getElementById('Location');
-var minCustomersEl = document.getElementById('Mininmum Customer');
-var maxCustomersEl = document.getElementById('Maximum Customer');
-var avgCustsalesEl = document.getElementById('Average Cookie Sales');
-var storeCountEl = document.getElementById('Store Count');
-var formElement = document.getElementById('test-form');
-var storeInformationSheet =  [] /// Array I'm passing to form element
+//  var body = document.getElementById('table-body');
+ var hoursButton = document.getElementById('hours');
+ var locationEl = document.getElementById('Location');
+ var minCustomersEl = document.getElementById('Mininmum Customer');
+ var maxCustomersEl = document.getElementById('Maximum Customer');
+ var avgCustsalesEl = document.getElementById('Average Cookie Sales');
+ var formElement = document.getElementById('test-form');
+ var storeInformationSheet = [] /// Array I'm passing to form element
 
 //------------------------------------------------------------------------------------
 
@@ -31,19 +30,22 @@ storeBuilder.prototype.salesResults = function() {
   var SaleSheet = []
   for (var i = 0; i < this.storeHours.length; i++) {
     console.log()
-      var randomAmount = (Math.floor(Math.random() * (this.minCust - this.maxCust + 1)) + this.minCust)
-      console.log(this.maxCust, this.minCust, randomAmount)
+        var randomAmount = Math.floor(Math.random() * (Math.floor(this.maxCust) - Math.ceil(this.minCust)) + Math.ceil(this.minCust));
+        
 
-      SaleSheet[i] = Math.round((randomAmount*this.avgSales)); // mutation of our object 
-      totalShiftsales = SaleSheet[i] + totalShiftsales  
+      console.log(randomAmount)
+        SaleSheet[i] = Math.round(randomAmount*this.avgSales); // mutation of our object 
+    
+    totalShiftsales = SaleSheet[i] + totalShiftsales  
   }
   this.hourlySales = SaleSheet
   this.totalSale = totalShiftsales
+ 
 }
 
 // Writes Data
-storeBuilder.prototype.salesData = function() {
-  var table = document.createElement('table')
+storeBuilder.prototype.salesData = function () {
+  // var table = document.createElement('table')
   var body = document.getElementById('table-body');
   var tableRow = document.createElement('tr')
   var heading = document.createElement('th')
@@ -53,67 +55,62 @@ storeBuilder.prototype.salesData = function() {
   heading.textContent = this.storeName;
   tableRow.appendChild(heading)
 
-  for(var i = 0; i <= this.hourlySales.length; i++ ) {
+  for (var i = 0; i <= this.hourlySales.length; i++) {
 
-   var dataCell = document.createElement('td')
-   dataCell.textContent = this.hourlySales[i];
-   tableRow.appendChild(dataCell)
+    var dataCell = document.createElement('td')
+    dataCell.textContent = this.hourlySales[i];
+    tableRow.appendChild(dataCell)
   }
-   dataCell.textContent = this.totalSale;
-   tableRow.appendChild(dataCell)
-   table.appendChild(tableRow)
-   body.appendChild(table)
-
+  dataCell.textContent = this.totalSale;
+  tableRow.appendChild(dataCell)
+  body.appendChild(tableRow);
 }
 
 // Document Writer for Heading Information
-function display(operationHours) {
+hoursButton.addEventListener('click', function () {
+
+  var headingHours = ["6am", "7am", "8am", "9am", "10am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm" ]
+
   var heading = document.createElement('th')
   var tableRow = document.createElement('tr')
-  var table = document.createElement('table')
   var body = document.getElementById('table-body');
 
   heading.textContent = ' '
   tableRow.appendChild(heading)
 
-  for (var i = 0; i <= operationHours.length; i++) {
+  for (var i = 0; i <= headingHours.length; i++) {
     var heading = document.createElement('th')
 
-    heading.textContent = operationHours[i];
+
+    console.log(headingHours[i])
+
+    heading.textContent = headingHours[i];
     tableRow.appendChild(heading)
   }
   heading.textContent = 'Total';
-  tableRow.appendChild(heading)
-  table.appendChild(tableRow)
-  body.appendChild(table)
-}
+  tableRow.appendChild(heading);
+  body.appendChild(tableRow);
+
+})
+
 
 // Takes Information from boxes and creates array
  formElement.addEventListener('submit', function (event) {
   event.preventDefault(); // we need this to stop forms from refreshing
 
-
- 
-  // var location = event.target.location.value
-  // var minCustomers = event.target.minCustomers.value
-  // var maxCustomers = event.target.maxCustomers.value
-  // var avgCustsale = event.target.avgCustsale.value
+  var location = event.target.location.value
+  var minCustomers = event.target.minCustomers.value
+  var maxCustomers = event.target.maxCustomers.value
+  var avgCustsale = event.target.avgCustsale.value
   var storeHoursLocation = ["6am", "7am", "8am", "9am", "10am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm" ]
+ 
 
-   
-var location = ["Seattle", "Tokyo", "Dubai", "Paris", "Lima"]
-var minCustomers =  [65, 24, 38, 38, 16]
-var maxCustomers = [23, 3, 11, 20, 2]
-var avgCustsale = [6.3, 1.2, 3.7, 2.3, 4.6]
-
-
-   display(storeHoursLocation)
-
-   for (var i = 0; i < location.length; i++) {
-    var StoreConstructor = new storeBuilder(location[i], minCustomers[i], maxCustomers[i], avgCustsale[i], storeHoursLocation)
+   parseInt(minCustomers)
+   parseInt(maxCustomers)
+   parseInt(avgCustsale)
 
   // Builds
-  //  var StoreConstructor = new storeBuilder(location, minCustomers, maxCustomers, avgCustsale, storeHoursLocation)
+   var StoreConstructor = new storeBuilder(location, minCustomers, maxCustomers, avgCustsale, storeHoursLocation)
   // Calculates
    StoreConstructor.salesResults()
   // Spits out Data
@@ -121,8 +118,42 @@ var avgCustsale = [6.3, 1.2, 3.7, 2.3, 4.6]
   // Pushes
    storeInformationSheet.push(StoreConstructor);
    
-  }
+  // }
     
  })
 
 
+// //
+//  hoursButton.addEventListener('click', function () {
+
+//   var headingHours = ["6am", "7am", "8am", "9am", "10am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm" ]
+
+//   var heading = document.createElement('th')
+//   var tableRow = document.createElement('tr')
+//   var body = document.getElementById('table-body');
+
+//   heading.textContent = ' '
+//   tableRow.appendChild(heading)
+
+//   for (var i = 0; i <= headingHours.length; i++) {
+//     var heading = document.createElement('th')
+
+
+//     console.log(headingHours[i])
+
+//     heading.textContent = headingHours[i];
+//     tableRow.appendChild(heading)
+//   }
+//   heading.textContent = 'Total';
+//   tableRow.appendChild(heading);
+//   body.appendChild(tableRow);
+
+
+// document.write('<h3>' + greeting + '</h3>');
+
+
+
+
+
+ 
+ 
